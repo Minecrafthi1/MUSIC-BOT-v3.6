9b4e62c532a9d05281f6dc8b6e530523667c455d
Minecrafthi1 <mimecrafthi30@gmail.com>
2024-07-01 16:59:27 +0800

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -37,7 +37,7 @@ module.exports = {
             .setAuthor({
           name: 'Owner',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/7Y7nX5mJQb'
         })
             .setDescription(`__**About me**__:\n\n ▶️ Myself Earl aka woznyuk. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [Earl](${youtubeLink})\n Instagram : 💙 [Earl](${DiscordLink})`)
             .setTimestamp();
